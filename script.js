d480ea3fc5482328bdf2da565fe40e9cf2af4f15
Michael Mesereau <mmesereau1@gmail.com>
2016-05-12 13:24:18 -0600

    Checkerboard pt 4

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-// Your JS goes here'use strict';
 'use strict';
 
-var red = 25;
-var blue = 150;
 
 var body = document.body;
-for (var i = 0; i < 81; i++){
+
+for (var i = 0; i < 100; i++){
   var newDiv = document.createElement('div');
   body.appendChild(newDiv);
-  newDiv.style.width = "11.1%";
+  newDiv.style.width = "10%";
   newDiv.style.cssFloat = "left";
-  newDiv.style.paddingBottom = "11.1%";
-  red += 3;
-  if (i % 2 == 0) {
-    var green = 100;
-  }
-  else {
-    var green = 130;
-  }
-//this is a comment
+  newDiv.style.paddingBottom = "10%";
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
   newDiv.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
 }
+setInterval(function() {
+  for (i = 0; i < body.children.length; i++) {
+    var myThing = body.children[i];
+    console.log(myThing);
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    myThing.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+  }
+}, 5);
+window.addEventListener("click", function() {
+    alert("You clicked on the page!");
+});
