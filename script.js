e904d3a7eaa1d28eb129a82d5e54ef3e88ead080
Michael Mesereau <mmesereau1@gmail.com>
2016-05-12 13:21:43 -0600

    Checkerboard pt 2

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 'use strict';
 
+
 var body = document.body;
 for (var i = 0; i < 81; i++){
   var newDiv = document.createElement('div');
   body.appendChild(newDiv);
-  // Each tile should be a div
-// Each tile's width is 11.1%
-// Set each tile's float property to left
-// Each tile's paddingBottom is 11.1%
   newDiv.style.width = "11.1%";
   newDiv.style.cssFloat = "left";
   newDiv.style.paddingBottom = "11.1%";
-  if (i % 2 == 0) {
-    newDiv.style.backgroundColor = "black";
-  }
-  else {
-    newDiv.style.backgroundColor = "red";
-  }
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  newDiv.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
 }
